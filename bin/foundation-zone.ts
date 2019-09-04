8f71b6492704965c9d0da79d4e3942715ef449dc
Debanjan Basu <debanjan.basu@ac3.com.au>
2019-09-04 12:10:16 +1000

    updated stacks added rds

--- bin/foundation-zone.ts
+++ bin/foundation-zone.ts
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
 import { App } from "@aws-cdk/core";
-import { FoundationZoneStack } from "../lib/foundation-zone-stack";
+import { FoundationZoneVPC } from "../lib/foundation-zone-vpc";
+import { WordpressRDS } from "../lib/wordpress-db/wordpress-rds";
 
-const app = new App();
+const ENV = {
+  region: "ap-southeast-2",
+  account: "069005529580"
+};
 
-new FoundationZoneStack(app, "FoundationZoneStack");
+class MyApp extends App {
+  constructor() {
+    super();
+    new FoundationZoneVPC(this, "FoundationZoneVPC", {
+      env: ENV
+    });
+    new WordpressRDS(this, "WordpressRDS", {
+      env: ENV
+    });
+  }
+}
+
+new MyApp().synth();
--- lib/foundation-zone-stack.ts
+++
@@ -1,9 +0,0 @@
-import { Stack, App, StackProps } from "@aws-cdk/core";
-
-export class FoundationZoneStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    console.log("Hello World");
-  }
-}
